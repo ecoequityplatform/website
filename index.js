fffb2d27897057c87bac4916f203076b5b9758b3
Yuliia Shevchenko <133769004+j-sheva@users.noreply.github.com>
2024-10-17 12:08:30 +0200

    done

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const teamMembers = [
     David’s innovative approach to marketing was showcased in a London Underground campaign that placed EQIFi on 1100 billboards across the UK, successfully navigating strict financial advertising regulations to boost brand awareness.
 
     His thought leadership has been featured in prominent publications such as Entrepreneur, Yahoo Finance, and FinTech Futures, where his insights on blockchain and fintech strategy have made a significant impact on the global tech landscape. `,
-    imgSrc: '/images/david.png',
+    imgSrc: '/images/david.jpg',
     linkedin: '#',
     xUrl: '#',
   },
@@ -69,7 +69,7 @@ const teamMembers = [
     In 2018, Frank transitioned from his role at Google to embark on a new entrepreneurial path, founding his own digital marketing agency. His work has since focused on leveraging his extensive knowledge of blockchain and cryptocurrency, guiding several startups in these emerging fields. Frank's approach combines deep technical understanding with strategic marketing insight, making him a sought-after consultant for businesses looking to navigate the complexities of digital transformation.
 
     Outside of his professional pursuits, Frank is an avid sports enthusiast, an accomplished photographer, and a certified sommelier with a passion for wine tasting. His diverse interests and commitment to excellence in both his personal and professional life continue to drive his success and influence in the industry. `,
-    imgSrc: '/images/frank.png',
+    imgSrc: '/images/frank.jpg',
     linkedin: '#',
     xUrl: '#',
   },
@@ -79,7 +79,7 @@ const teamMembers = [
     description: `Stefano Tempesta is a technologist working at the crossroad of Web2 and Web3, to make the Internet a more accessible, meaningful, and inclusive space. Stefano is an ambassador of the use of AI and blockchain technologies for good humanitarian purposes. A former advisor to the Department of Industry and Science, Australia, on the National Blockchain Roadmap, he is currently a lecturer at RMIT University, Australia, of courses about AI and blockchain technology.
 
 A passionate traveller, a poor musician, and an avid learner of new technologies and (programming) languages, Stefano has co-authored the books "Blockchain Applied" and "Developing Blockchain Applications in the Cloud", holds three citizenships, and speaks fluent English, Italian, and terrible Ukrainian.`,
-    imgSrc: '/images/stefano.png',
+    imgSrc: '/images/stefano.jpg',
     linkedin: '#',
     xUrl: '#',
   },
@@ -87,7 +87,7 @@ A passionate traveller, a poor musician, and an avid learner of new technologies
     name: 'Chase Bradshaw',
     title: 'Chief Financial Officer',
     description: `Chase is an experienced finance leader specializing in early-stage, hyper-growth SaaS startups. With a proven track record in strategic business planning, he drives both short- and long-term growth strategies by modeling financial impacts and making data-driven recommendations that enhance profitability and cash management. He excels at establishing robust financial controls to ensure compliance, audit success, and operational efficiency. Chase is also skilled in implementing precise financial reporting systems that consistently deliver accurate forecasts. Additionally, he recruits and develops top-tier finance talent to build high-performing teams.`,
-    imgSrc: '/images/chase.png',
+    imgSrc: '/images/chase.jpg',
     linkedin: '#',
     xUrl: '#',
   },
@@ -97,7 +97,7 @@ A passionate traveller, a poor musician, and an avid learner of new technologies
     description: `Gregory Vint is a corporate lawyer with experience at major corporations and financial institutions in the US and EU.  He was also an attorney at the FDIC, a US banking regulatory, where he advised policy makers on legal issues related to new regulatory initiatives.  After relocating from the US to Estonia in 2018, he worked with EU companies seeking to expand in the US, including a FinTech company that has achieved "unicorn" status.  He also worked on the institutional fixed income desk at JP Morgan advising clients and collaborating with traders to structure trading and investment strategies for large fixed income investors.  Notable legal areas Gregory has addressed include: analyzing the relationship between cryptocurrency and blockchain in the financial industry with US financial laws and regulations, impact of regulatory initiatives on FinTechs and emerging technologies in financial markets, and corporate financing and structuring.
 
  He has a B.A. from Rice University and a J.D. from the University of Texas School of Law.  He is an active member of the State Bar of Texas and completed Level 1 of the Chartered Financial Analyst certification program.`,
-    imgSrc: '/images/gregory.png',
+    imgSrc: '/images/gregory.jpg',
     linkedin: '#',
     xUrl: '#',
   },
@@ -105,7 +105,7 @@ A passionate traveller, a poor musician, and an avid learner of new technologies
     name: 'Yuliia Shevchenko',
     title: 'UX/UI Design Lead',
     description: `Yuliia is a passionate UX/UI designer focused on creating digital experiences that make an impact. Design is more than just a profession—it's a way to merge creativity and functionality into seamless, user-friendly designs. From mobile apps to enterprise software, Yuliia is dedicated to delivering modern, immaculate designs of the highest standard. With a human-centered approach, Yuliia empathizes with users, identifies their pain points, and crafts solutions that enhance their experience with your brand or product. A lover of travel and living in different countries, Yuliia draws inspiration from diverse cultures to enrich the design process.`,
-    imgSrc: '/images/yuliia.png',
+    imgSrc: '/images/yuliia.jpg',
     linkedin: 'https://www.linkedin.com/in/uliia-shevchenko/',
     xUrl: '#',
   },
@@ -113,7 +113,7 @@ A passionate traveller, a poor musician, and an avid learner of new technologies
     name: 'Sarah Petrik',
     title: 'Head of Recruitment',
     description: ``,
-    imgSrc: '/images/sarah.png',
+    imgSrc: '/images/sarah.jpg',
     linkedin: '#',
     xUrl: '#',
   },
@@ -123,7 +123,7 @@ A passionate traveller, a poor musician, and an avid learner of new technologies
     description: `Kevin Tome is a Brand/Visual Designer based in Kitchener-Waterloo with a strong background in brand design, marketing, and content creation. He has a proven track record of leading projects, mentoring junior designers, and collaborating closely with clients. Kevin also teaches part-time at his local college, shaping the next generation of designers.
 
 In his recent work, Kevin led branding initiatives in the web3 space at Airfoil, developing brand systems for various organizations. At ApplyBoard, he helped international students gain access to quality education by managing a wide array of design projects. His experience also includes roles at various companies where he contributed to branding and marketing efforts.`,
-    imgSrc: '/images/kevin.png',
+    imgSrc: '/images/kevin.jpg',
     linkedin: '#',
     xUrl: '#',
   },
@@ -137,7 +137,9 @@ function renderTeamMembers() {
     itemDiv.classList.add('slick__item');
     itemDiv.innerHTML = `
           <div class="team__item">
-              <img class="team__img" src="${member.imgSrc}" alt="${member.name}" />
+          <div class="team__img-wrapper">
+
+              <img class="team__img" src="${member.imgSrc}" alt="${member.name}" /></div>
               <span class="team__title">${member.title}</span>
               <p class="team__name">${member.name}</p>
               <div class="team__actions">
